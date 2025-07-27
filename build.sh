73562b2fc20edd8d673e0dc9fc90ec93f125f195
Juan125-ai <casillasmja5@gmail.com>
2025-07-27 17:36:35 +0100

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pip install Flask==3.0.0
+pip install gunicorn==21.2.0
+pip install openai==1.30.1
+pip install python-dotenv==1.0.0
+pip install pandas==2.2.2
+pip install numpy==1.26.4
+pip install tiktoken==0.9.0
+pip install requests==2.32.4
